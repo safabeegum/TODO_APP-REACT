8074d14481faa50273557af75249a935d7416aeb
safabeegum <pasafabeegum@gmail.com>
2024-06-08 11:54:42 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import AddTask from './components/AddTask';
 import SearchTask from './components/SearchTask';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ViewTask from './components/ViewTask';
+import DeleteTask from './components/DeleteTask';
 
 function App() {
   return (
     <div>
-      <AddTask/>
-      <SearchTask/>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AddTask/>}/>
+        <Route path='/search' element={<SearchTask/>}/>
+        <Route path='/delete' element={<DeleteTask/>}/>
+        <Route path='/viewall' element={<ViewTask/>}/>
+        </Routes></BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
         <nav class="navbar navbar-expand-lg bg-danger">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">TODO</a>
+    <Link class="navbar-brand" href="#">TODO</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="#">ADD</a>
+          <Link class="nav-link" to="/">ADD</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">SEARCH</a>
+          <Link class="nav-link" to="/search">SEARCH</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">DELETE</a>
+          <Link class="nav-link" to="/delete">DELETE</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">VIEW</a>
+          <Link class="nav-link" to="/viewall">VIEW</Link>
         </li>
       </ul>
     </div>
