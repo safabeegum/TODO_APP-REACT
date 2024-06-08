27a2d86ea94dc2fe991f87bc6f0b65f07599b69d
safabeegum <pasafabeegum@gmail.com>
2024-06-08 06:12:48 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddTask from './components/AddTask';
+import SearchTask from './components/SearchTask';
 
 function App() {
   return (
     <div>
       <AddTask/>
+      <SearchTask/>
     </div>
   );
 }
