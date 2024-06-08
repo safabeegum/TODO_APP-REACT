9ba1ba50bbcbd02462e12dc72b86538069e3db24
safabeegum <pasafabeegum@gmail.com>
2024-06-08 06:09:29 +0530

    Completed

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+const AddTask = () => {
+  return (
+    <div><div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">TASK ID</label>
+                    <input type="text" className="form-control" />
+                </div>
+
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">TASK NAME</label>
+                    <input type="text" className="form-control" />
+                </div>
+
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label htmlFor="" className="form-label">DESCRIPTION</label>
+                    <textarea name="" id="" className="form-control"></textarea>
+                </div>
+
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">TYPE</label>
+                    <input type="text" className="form-control" />
+                </div>
+
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">PURPOSE</label>
+                    <input type="text" className="form-control" />
+                </div>
+
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">START DATE</label>
+                    <input type="date" name="" id="" className="form-control" />
+                </div>
+
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">END DATE</label>
+                    <input type="date" name="" id="" className="form-control" />
+                </div>
+
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <button className="btn btn-success">SUCCESS</button>
+                </div>
+
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <button className="btn btn-secondary">RESET</button>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div></div>
+  )
+}
+
+export default AddTask
